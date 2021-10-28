4be0c70581379e34fe2c10688433f36b56953988
Peter Abbott <peter@atomic.io>
2021-10-29 10:16:27 +1300

    more testing

--- localstack/init/010-lambdas.sh
+++ localstack/init/010-lambdas.sh
@@ -1,1 +1,24 @@
+aws --endpoint-url http://localstack:4566 s3 cp /assets/placeholder.zip s3://lambdas/ 
+
+#!/bin/sh
+
+## NOTE: timeout of 200 to get around performance issues
+
+makeLambda() {
+  FUNC_NAME=$1
+  aws --endpoint-url http://localstack:4566 lambda create-function \
+    --function-name "$FUNC_NAME" \
+    --code "S3Bucket=lambdas,S3Key=placeholder.zip" \
+    --handler src/handler.handler \
+    --runtime 'nodejs14.x' \
+    --timeout 200 \
+    --role dummy
+
+  aws --endpoint-url http://localstack:4566 lambda put-function-concurrency \
+    --function-name "$FUNC_NAME" \
+    --reserved-concurrent-executions 1
+}
+
+makeLambda "test-processor"
+
 aws --endpoint-url http://localstack:4566 lambda list-functions
